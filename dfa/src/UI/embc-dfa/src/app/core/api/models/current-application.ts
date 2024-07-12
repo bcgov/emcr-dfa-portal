e5b45c21d595b33da2455cc9192c6da791b0dbde
W. Ross White <ross.white@quartech.com>
2024-07-12 14:31:03 -0700

    fgured out sass issue finally

--- dfa/src/UI/embc-dfa/src/app/core/api/models/current-application.ts
+++ dfa/src/UI/embc-dfa/src/app/core/api/models/current-application.ts
@@ -3,7 +3,6 @@
 import { StatusBar } from './status-bar';
 export interface CurrentApplication {
   applicationId?: string;
-  applicationSubType?: string;
   applicationType?: string;
   caseNumber?: string;
   damagedAddress?: string;
@@ -11,7 +10,6 @@ export interface CurrentApplication {
   dateOfDamage?: string;
   eventId?: string;
   isErrorInStatus?: boolean;
-  legalName?: string;
   primaryApplicantSignedDate?: string;
   status?: string;
   statusBar?: Array<StatusBar>;
