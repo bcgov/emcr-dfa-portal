eb7002fd25c7d274dbcc6631f8c99fbf627d4faf
claudiu <Claudiu.Vlasceanu@nttdata.com>
2024-09-05 16:08:43 -0700

    add tests for create new app and interact with front page

---
+++ DFAautomationTests/src/test/java/CreateNewApplicationPublic.java
@@ -0,0 +1,95 @@
+import dfa.CustomWebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+import static dfa.CustomWebDriverManager.getDriver;
+
+public class CreateNewApplicationPublic {
+
+    private WebDriver driver;
+
+
+//    @After
+//    public void tearDown() {
+//        driver.close();
+//        driver.quit();
+//    }
+//
+//    @AfterClass
+//    public static void afterClass() {
+//        CustomWebDriverManager.instance = null;
+//    }
+
+
+    @Test
+    public void test() throws Exception {
+        driver = getDriver();
+        WebDriverWait driverWait = CustomWebDriverManager.getDriverWait();
+        WebElement element = CustomWebDriverManager.getElement();
+        CustomWebDriverManager.getElements();
+
+
+        LoginPublicPortal loginPublicPortal = new LoginPublicPortal();
+        loginPublicPortal.test();
+
+        //Create New application
+        element = driverWait.until(ExpectedConditions
+                .presenceOfElementLocated(By.xpath("//*[contains(text(), ' Create a New Application ')]")));
+        element.click();
+
+        //Select dates
+        Calendar calendar = Calendar.getInstance();
+        Date today = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date tomorrow = calendar.getTime();
+
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/YYYY");
+
+        String todayAsString = dateFormat.format(today);
+        String tomorrowAsString = dateFormat.format(tomorrow);
+
+        System.out.println(todayAsString);
+        System.out.println(tomorrowAsString);
+
+        //Select cause of damage
+        Thread.sleep(1000);
+        String[] checkboxIds = {
+                "mat-mdc-checkbox-1-input",
+                "mat-mdc-checkbox-2-input",
+                "mat-mdc-checkbox-3-input",
+                "mat-mdc-checkbox-4-input",
+                "mat-mdc-checkbox-5-input"
+        };
+
+
+        for (String id : checkboxIds) {
+            element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
+            ElementClickHelper.clickElement(driver, element);
+        }
+
+        Random r = new Random();
+        String alphabet = "123xyz";
+        StringBuilder randomChars = new StringBuilder();
+
+        for (int i = 0; i < 100; i++) {
+            randomChars.append(alphabet.charAt(r.nextInt(alphabet.length())));
+        }
+
+        // Cause of damage
+        element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-3")));
+        element.clear();
+        element.sendKeys(randomChars.toString());
+
+    }
+}
