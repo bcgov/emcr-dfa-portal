3779276c7c53837f74ca891c5d516761cf722cae
JustinMacaulay <justinkmacaulay@gmail.com>
2025-02-13 15:08:05 -0800

    Null the values of the project amendment if the amendment is not approved or approved with exclusions

--- dfa-public/src/UI/embc-dfa/src/app/feature-components/dfa-project-amendment/dfa-project-amendment.component.ts
+++ dfa-public/src/UI/embc-dfa/src/app/feature-components/dfa-project-amendment/dfa-project-amendment.component.ts
@@ -134,6 +134,7 @@ export class DFAProjectAmendmentComponent
 
     this.dfaProjectMainDataService.setApplicationId(this.appId);
     this.disableFormfields();
+    console.log(this.projectAmendmentForm.controls.amended18MonthDeadline.value);
   }
 
   disableFormfields(): void {
@@ -316,6 +317,16 @@ export class DFAProjectAmendmentComponent
       var jsonVal = JSON.stringify(this.items);
       this.isErrorInStatus = false;
       this.statusBar = JSON.parse(jsonVal);
+
+      if(objAmendment.amendmentDecision !== "Approved" && objAmendment.amendmentDecision !== "Approved with Exclusions") {
+        objAmendment.amendmentReceivedDate = null
+        objAmendment.emcrDecisionComments = null
+        objAmendment.deadlineExtensionApproved = null
+        objAmendment.amended18MonthDeadline = null
+        objAmendment.additionalProjectCostDecision = null
+        objAmendment.approvedAdditionalProjectCost = null
+      }
+
       this.statusBar.forEach(objStatItem => {
         if (objAmendment.status != null && objStatItem.status.toLowerCase() == objAmendment.status.toLowerCase()) {
           objStatItem.currentStep = true;
