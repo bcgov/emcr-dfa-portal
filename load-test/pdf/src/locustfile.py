4a6240431a8c35957c24ad67c355a4bb0f91fd44
AJ Po-Déziel <alainjoseph.podeziel@gov.bc.ca>
2025-02-20 19:17:35 -0800

    Update code documentation for specificity and functionality

--- load-test/pdf/src/locustfile.py
+++ load-test/pdf/src/locustfile.py
@@ -1,15 +1,15 @@
 from locust import HttpUser, task, between
 
-# GetPDF_DFATest
-
 class User(HttpUser):
     @task
     def post_pdf(self):
         headers = { 'content-type': 'application/json' }
+
+        # Users will target GetPDF_DFATest() function in pdf-service/Controllers/PDFController.cs
         response = self.client.post("/api/pdf/GetPDF_DFATest", headers=headers)
         
-        # Optionally, check that the response status is 200
+        # Verify if response returns 200, otherwise print unexpected HTTP status code.
         assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
         
-        # You can also print response details for debugging
+        # Print response details for debugging
         # print(response.text)
