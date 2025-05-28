14c6d3faa4fe40b8d4238410f70664aa648b75a4
Jebb Burditt <jebb.burditt@gmail.com>
2025-05-28 14:48:33 -0700

    clean up feature enabled directive

--- dfa-public/src/UI/embc-dfa/src/app/core/directives/feature-enabled.directive.ts
+++ dfa-public/src/UI/embc-dfa/src/app/core/directives/feature-enabled.directive.ts
@@ -2,23 +2,22 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { ConfigService } from '../services/config.service';
 
 @Directive({
-  selector: '[featureRemove]',
+  selector: '[featureEnabled]',
   standalone: true
 })
-export class FeatureRemoveDirective implements OnInit {
-  @Input('featureRemove') featureName: string;
-  //@Input('featureRemoveIf') featureRemoveIf: boolean = true;
+export class FeatureEnabledDirective implements OnInit {
+  @Input('featureEnabled') featureName: string;
+  @Input('featureEnabledIf') featureEnabledIf: boolean = true;
 
   constructor(private el: ElementRef, private configService: ConfigService) {
-    console.log('FeatureRemoveDirective initialized');
+    console.log('featureEnabledDirective initialized');
  }
 
   ngOnInit() {
-    //console.log(`Checking feature flag: ${this.featureName}, expected: ${this.featureRemoveIf}`);
-    console.log(`Checking feature flag: ${this.featureName}`);
+    console.log(`Checking feature flag: ${this.featureName}, expected: ${this.featureEnabledIf}`);
     console.log(`Feature flag value: ${this.configService.configuration.featureFlags.useAppeals}`);
     //if (this.featureFlagService.feature(this.featureName) == true) {
-    if (!this.configService.configuration.featureFlags.useAppeals) {
+    if (this.configService.configuration.featureFlags.useAppeals !== this.featureEnabledIf) {
       this.el.nativeElement.parentNode.removeChild(this.el.nativeElement);
     }
   }
