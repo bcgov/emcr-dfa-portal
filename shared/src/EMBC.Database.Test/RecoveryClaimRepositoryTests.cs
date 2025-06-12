7c44d7bf0b308b007c4bea0e62b5b1405837ee84
Jebb Burditt <jebb.burditt@gmail.com>
2025-06-12 16:04:01 -0700

    add where unit test

--- shared/src/EMBC.Database.Test/RecoveryClaimRepositoryTests.cs
+++ shared/src/EMBC.Database.Test/RecoveryClaimRepositoryTests.cs
@@ -1,4 +1,4 @@
-﻿public class RecoveryClaimRepositoryTests(IRecoveryClaimRepository repository/*, IRecoveryClaimService recoveryClaimService*/, IMapper mapper)
+﻿public class RecoveryClaimRepositoryTests(IRecoveryClaimRepository repository, IMapper mapper)
 {
     // WARNING these are not valid unit tests and depend on data in Dynamics not changing, use for local testing purposes only
 
@@ -9,10 +9,10 @@
         {
             //IncludeChildren = true,
             //Id = new Guid("<guid>"),
-            //CodingBlockSubmissionStatus = CodingBlockSubmissionStatus.PendingSubmission,
-            //AfterInvoiceDate = new DateTime(2023, 1, 1),
-            //AfterDateGoodsReceived = new DateTime(2023, 1, 1),
-            //AfterDateInvoiceReceived = new DateTime(2023, 1, 1)
+            CodingBlockSubmissionStatus = CodingBlockSubmissionStatus.PendingSubmission,
+            AfterInvoiceDate = new DateTime(2023, 1, 1),
+            AfterDateGoodsReceived = new DateTime(2023, 1, 1),
+            AfterDateInvoiceReceived = new DateTime(2023, 1, 1)
         };
         var recoveryClaims = repository.Query(query);
 
@@ -21,40 +21,24 @@
     }
 
     //[Fact]
-    //public async Task GetPending_Success()
+    //public void Update_Success()
     //{
-    //    var responses = await recoveryClaimService.ProcessClaims();
+    //    var id = new Guid("a33ca996-86cd-ec11-b832-00505683fbf4");
 
-    //    Assert.NotNull(responses);
+    //    repository.Update(id, x => x.CodingBlockSubmissionStatus == CodingBlockSubmissionStatus.Failed);
+
+    //    var updatedClaim = repository
+    //        .Query(new RecoveryClaimQuery { Id = id })
+    //        .FirstOrDefault();
+    //    Assert.NotNull(updatedClaim);
+    //    Assert.Equal(CodingBlockSubmissionStatus.Failed, updatedClaim.CodingBlockSubmissionStatus);
     //}
 
     [Fact]
-    public void UpdateCodingBlockSubmissionStatus_Success()
+    public void Where()
     {
-        var id = new Guid("<guid>");
-        var codingBlockSubmissionStatus = CodingBlockSubmissionStatus.Failed;
+        var results = repository.Where(x => x.CodingBlockSubmissionStatus == CodingBlockSubmissionStatus.PendingSubmission);
 
-        repository.UpdateCodingBlockSubmissionStatus(id, codingBlockSubmissionStatus);
-
-        var updatedClaim = repository
-            .Query(new RecoveryClaimQuery { Id = id })
-            .FirstOrDefault();
-        Assert.NotNull(updatedClaim);
-        Assert.Equal(codingBlockSubmissionStatus, updatedClaim.CodingBlockSubmissionStatus);
-    }
-
-    [Fact]
-    public void UpdateCodingBlockSubmissionFailure_Success()
-    {
-        var id = new Guid("<guid>");
-        var errorMessage = "An error occurred while processing the request. Our system was unable to successfully perform the request via the API gateway and/or the OpenShift service. Please check the plugins trace logs for more details.";
-
-        repository.UpdateFailure(id, errorMessage);
-
-        var updatedClaim = repository
-            .Query(new RecoveryClaimQuery { Id = id })
-            .FirstOrDefault();
-        Assert.NotNull(updatedClaim);
-        Assert.Equal(errorMessage, updatedClaim.LastCodingBlockSubmissionError);
+        Assert.NotEmpty(results);
     }
 }
