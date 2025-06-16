8224132a27b6cbe72116f667f997c9b660855af1
Jebb Burditt <jebb.burditt@gmail.com>
2025-06-16 09:25:24 -0700

    mask guid

--- shared/src/EMBC.Database.Test/RecoveryClaimRepositoryTests.cs
+++ shared/src/EMBC.Database.Test/RecoveryClaimRepositoryTests.cs
@@ -46,7 +46,7 @@
     public void Update_Dto_Multiple_Success()
     {
         var recoveryClaim = new RecoveryClaim();
-        recoveryClaim.Id = new Guid("a33ca996-86cd-ec11-b832-00505683fbf4");
+        recoveryClaim.Id = new Guid("<guid>");
         recoveryClaim.CodingBlockSubmissionStatus = CodingBlockSubmissionStatus.Failed;
         recoveryClaim.PaymentAdviceComments = "TEST";
 
