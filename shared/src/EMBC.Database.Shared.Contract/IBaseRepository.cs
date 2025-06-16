1ff2839470f46502375645a3c006dfeb34ef7984
Jebb Burditt <jebb.burditt@gmail.com>
2025-06-16 09:11:04 -0700

    missed the interface change to match previous commit

--- shared/src/EMBC.Database.Shared.Contract/IBaseRepository.cs
+++ shared/src/EMBC.Database.Shared.Contract/IBaseRepository.cs
@@ -8,7 +8,6 @@ public interface IBaseRepository<TDto> where TDto : IDto
     IEnumerable<TDto> Where(Expression<Func<TDto, bool>> predicates);
     bool Update(TDto dto);
     bool Update(TDto dto, params Expression<Func<TDto, object>>[] properties);
-    bool Update(Guid id, params Expression<Func<TDto, object>>[] properties);
     bool TryDelete(Guid id);
     bool TryDelete(TDto dto, bool isRecursive = false);
     bool TryDeleteRange(IEnumerable<TDto> invoices, bool isRecursive = false);
