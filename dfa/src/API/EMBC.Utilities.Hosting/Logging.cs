e73d437e6a7052fe306f5bc89f5985f5582604d7
Vlad Belukha <vlad.belukha@quartech.com>
2026-01-13 17:30:21 -0800

    [DEVOPS-632] logs version usage fixed

--- dfa/src/API/EMBC.Utilities.Hosting/Logging.cs
+++ dfa/src/API/EMBC.Utilities.Hosting/Logging.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
