44344e02090562eb763cd01d456e83878f205384
Vlad Belukha <vlad.belukha@quartech.com>
2026-01-13 17:18:22 -0800

    [DEVOPS-632] logs version fixed, UTC timestamp added

--- dfa/src/API/EMBC.Utilities.Hosting/Logging.cs
+++ dfa/src/API/EMBC.Utilities.Hosting/Logging.cs
@@ -35,7 +35,8 @@ namespace EMBC.Utilities.Hosting
                 //.Enrich.WithClientAgent()
                 .Enrich.WithClientIp()
                 .Enrich.WithSpan()
-                .Enrich.WithProperty("version", Environment.GetEnvironmentVariable("VERSION") ?? "unknown")
+                .Enrich.WithProperty("version", Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown")
+                .Enrich.WithProperty("UTC_Timestamp", DateTime.UtcNow.ToString("o"))
                 .Enrich.When(logEvent => IsSecurityEvent(logEvent), e => e.WithProperty("IsSecurityEvent", true))
                 .WriteTo.Console(outputTemplate: LogOutputTemplate);
 
